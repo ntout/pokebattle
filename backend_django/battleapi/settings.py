9770b62d2b080eb64cb4ad142a7cbfb7c16bc6bd
ntout <nathan.tout@oit.edu>
2022-01-27 19:43:56 -0800

    update requirements

--- backend_django/battleapi/settings.py
+++ backend_django/battleapi/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-i(gc0pmx2d1_mt@mw%orw*+s!jho-222pv@6)_r5(n4oye@*bb'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -64,7 +65,7 @@ REST_FRAMEWORK = {
 }
 
 CORS_ORIGIN_WHITELIST = (
-    'localhost:3000',
+    'https://localhost:3000',
 )
 
 ROOT_URLCONF = 'battleapi.urls'
@@ -91,8 +92,8 @@ DATABASES = {
    'default': {
        'ENGINE': 'django.db.backends.postgresql',
        'NAME': 'postgres',
-       'USER': 'postgres',
-       'PASSWORD': 'Pieces20Circle21',
+       'USER': config("DB_USER"),
+       'PASSWORD': config("DB_PASSWORD"),
        'HOST': 'localhost',
        'PORT': '5432',
    }
