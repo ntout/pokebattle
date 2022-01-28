5a0efd232eb2f0e68d239ebf6dd0502f6fa2e358
ntout <nathan.tout@oit.edu>
2022-01-28 14:19:15 -0800

    user app

---
+++ backend_django/users/serializers.py
@@ -0,0 +1,10 @@
+from dataclasses import field
+from rest_framework import serializers
+from django.contrib.auth.models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    model = User
+    fields = ('username')
+
+
